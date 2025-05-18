9aa0811e25beadbe729a09b1b8b4da23b3fda1cd
Yilfri Salave <yilfri97@hotmail.com>
2025-05-18 22:43:02 +0000

    start

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function random (options) {
+    let randomOption = Math.floor(Math.random() * options.length)
+    console.log(options + "-" + randomOption)
+    return options[randomOption] 
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = document.getElementById("excuse")
+  console.log("Hola")
+  excuse.innerHTML= `<h2>${random(who)} ${random(action)} ${random(what)} ${random(when)}</h2>`;
 };
